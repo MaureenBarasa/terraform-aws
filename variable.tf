2cc636d3b163691da7600c829a3607ce6e67d583
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-10-10 15:54:43 +0200

    modified variables

--- variable.tf
+++ variable.tf
@@ -15,9 +15,3 @@ variable "AWS_REGION" {
   type        = string
   default     = "eu-west-1"
 }
-
-variable "Name" {
-  description = "the name of the vpc"
-  type        = string
-  default     = "AWS-Practice"
-}
